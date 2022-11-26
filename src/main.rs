435191056436eb7704754019cc919d6295e085b2
Benedict Cairns <bcairns21@gmail.com>
2022-11-26 08:28:40 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let value = match args.get(1) {
+        None => 0,
+        Some(n) => n.parse::<u8>().unwrap_or(0)
+    };
+
+    match value {
+        n => {
+            println!("No entry for day {n}");
+        }
+    }
+}
