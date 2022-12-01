efbbd3595f1a549a061e680e2b4cefc9b9a004ee
Benedict Cairns <bcairns21@gmail.com>
2022-12-01 08:31:46 +0000

    Day 1 running framework

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
-use std::env;
+use std::{env, fs};
 mod puzzles;
+use puzzles::food;
+
+fn read_file(filename: &str) -> String {
+    match fs::read_to_string(filename.to_string()) {
+        Ok(v) => v,
+        Err(_) => panic!("No filename ${filename} found")
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,6 +18,11 @@ fn main() {
     };
 
     match value {
+        1 => {
+            let day1 = read_file("in1.txt");
+            let part1 = food::find_most_food(day1);
+            println!("Question 1: {part1:?}");
+        },
         n => {
             println!("No entry for day {n}");
         }
