b143c2a568045825e6307654cf02542b3a13ce23
Benedict Cairns <bcairns21@gmail.com>
2022-12-08 07:14:43 +0000

    Remove irrelevant comments

--- src/puzzles/filewalk.rs
+++ src/puzzles/filewalk.rs
@@ -48,10 +48,6 @@ fn build_path_name(address: &Vec<String>, name: &String) -> String{
 
 
 fn traverse_tree(input: &String) -> (WeightMap, WeightMap) {
-    // let mut sizes: HashMap<String, u32> = HashMap::new();
-    // let mut file_parents: HashMap<String, String> = HashMap::new();
-    // let mut dir_children: HashMap<String, Vec<String>> = HashMap::new();
-    // let mut current_node:  String = "/".to_string();
     let mut weight_stack : Vec<u32> = Vec::new();
     let mut address: Vec<String> = Vec::new();
     let mut weight_sum = 0;
@@ -66,18 +62,9 @@ fn traverse_tree(input: &String) -> (WeightMap, WeightMap) {
                 let full_addr = build_path_name(&address, &name);
                 weights.insert(full_addr, size);
                 weight_sum = weight_sum + size;
-                // sizes.insert(name.to_string(), size);
-                // file_parents.insert(name.to_string(), current_node.to_string());
-                // match dir_children.get_mut(&current_node) {
-                //     Some(v) => v.push(name.to_string()),
-                //     None => { dir_children.insert(current_node.to_string(), vec![name.to_string()]); }
-                // }
             },
-            Line::Directory(_name) => {
-                // Nothing yet
-            },
+            Line::Directory(_name) => (), // Do nothing
             Line::Move(directory) => {
-                // current_node = directory;
                 address.push(directory);
                 weight_stack.push(weight_sum);
                 weight_sum = 0;
@@ -87,9 +74,6 @@ fn traverse_tree(input: &String) -> (WeightMap, WeightMap) {
                 dir_weights.insert(full_addr, weight_sum);
                 address.pop();
                 weight_sum = weight_sum + weight_stack.pop().unwrap();
-                // let upper_name = file_parents.get(&current_node).unwrap();
-                // current_node = upper_name.to_string();
-                // current_node = *file_parents.get(&current_node.to_string()).unwrap().to_string();
             }
         }
     });
@@ -103,7 +87,6 @@ fn traverse_tree(input: &String) -> (WeightMap, WeightMap) {
     }
 
     (weights, dir_weights)
-    // (sizes, file_parents, dir_children)
 }
 
 pub fn biggest_small_dirs(input: &String) -> u32 {
@@ -113,8 +96,6 @@ pub fn biggest_small_dirs(input: &String) -> u32 {
 }
 
 pub fn smallest_big_dir(input: &String) -> u32 {
-    // 30_000_000
-    // 70_000_000
     let (_files, dirs) = traverse_tree(&input);
 
     let top_weight = dirs.get("/").unwrap();
