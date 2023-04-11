c11467cf3ad38bc30ff18e3e1c2c42a07b108822
Benedict Cairns <bcairns21@gmail.com>
2023-04-11 23:05:01 +0100

    Attempting a new solution method

--- src/puzzles/valves.rs
+++ src/puzzles/valves.rs
@@ -110,6 +110,109 @@ fn best_time_pressure(input: &String) -> u32 {
     max_score
 }
 
+fn graphify(path_map: &PathMap)  -> Vec<Vec<u32>>{
+    let size = path_map.len();
+
+    let idx_map: HashMap<String, usize> = path_map.iter()
+        .enumerate()
+        .fold(HashMap::new(), |mut map, (index, (name, _))| {
+            map.insert(name.clone(), index);
+            map
+        });
+
+    let mut graph = vec![vec![u32::MAX / 4; size]; size];
+
+    path_map.iter().enumerate().for_each(|(i, (_name, paths))| {
+        paths.iter().for_each(|s| {
+            let address = *idx_map.get(s).unwrap();
+            graph[i][address] = 1; // Direct link exists!
+        })
+    });
+
+    graph
+}
+
+fn floyd_warshall_roy(graph: Vec<Vec<u32>>) -> Vec<Vec<u32>> {
+    let l = graph.len();
+    let mut distance = graph.clone();
+
+    for k in 0..l {
+        for i in 0..l {
+            for j in 0..l {
+                if distance[i][k] + distance[k][j] < distance[i][j] {
+                    distance[i][j] = distance[i][k] + distance[k][j];
+                }
+            }
+        }
+    }
+
+    distance
+}
+
+fn magical_calculation(input: &String) -> u32 {
+    let (flows, paths) = read_valves(input);
+    let graph = graphify(&paths);
+    let distances = floyd_warshall_roy(graph);
+
+    let idx_map: HashMap<String, usize> = paths.iter()
+        .enumerate()
+        .fold(HashMap::new(), |mut map, (index, (name, _))| {
+            map.insert(name.clone(), index);
+            map
+        });
+
+    let scored_nodes: Vec<String> = flows.iter()
+        .filter(|(_name, score)| **score>0)
+        .map(|(name, _score)| name.clone()).collect();
+        // Find optimal path through all point-scoring nodes
+    
+    let start_index = *idx_map.get("AA").unwrap();
+
+    travelling_salesman(&flows, &scored_nodes, &distances, &idx_map, 30, 0, start_index, 0)
+}
+
+fn travelling_salesman(
+    flows: &FlowMap,
+    scored_nodes: &Vec<String>,
+    distances: &Vec<Vec<u32>>,
+    idx_map: &HashMap<String, usize>,
+    minutes: u32,
+    flow: u32,
+    current_index: usize,
+    state: u64
+) -> u32 {
+
+    let mut max_flow = flow;
+
+    // println!("{:b}", state);
+
+    for name in scored_nodes.iter() {
+        let new_index: usize = *idx_map.get(name).unwrap();
+        let current_minutes = minutes
+            .checked_sub(distances[current_index][new_index])
+            .and_then(|x| x.checked_sub(1))
+            .unwrap_or(0);
+
+        if 
+            state & (1 << new_index) == 1   // Already switched on
+            || current_minutes <= 0         // Can't reach in time
+        {
+            // Don't care about this node
+            continue;
+        }
+
+        // dbg!(name);
+        let new_state = state | (1 << new_index);
+        let new_flow = flow + (current_minutes * flows.get(name).unwrap());
+
+        max_flow = max_flow.max(
+            travelling_salesman(flows, scored_nodes, distances, idx_map, current_minutes, new_flow, new_index, new_state)
+        )
+    }
+
+    max_flow
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -129,5 +232,51 @@ mod tests {
         Valve JJ has flow rate=21; tunnel leads to valve II".to_string();
 
         assert_eq!(best_time_pressure(&input), 1651);
+    }
+
+    #[test]
+    fn read_test() {
+        let input = r"
+        Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+        Valve BB has flow rate=13; tunnels lead to valves CC, AA
+        Valve CC has flow rate=2; tunnels lead to valves DD, BB
+        Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+        Valve EE has flow rate=3; tunnels lead to valves FF, DD
+        Valve FF has flow rate=0; tunnels lead to valves EE, GG
+        Valve GG has flow rate=0; tunnels lead to valves FF, HH
+        Valve HH has flow rate=22; tunnel leads to valve GG
+        Valve II has flow rate=0; tunnels lead to valves AA, JJ
+        Valve JJ has flow rate=21; tunnel leads to valve II".to_string();
+
+        let (_flow, paths) = read_valves(&input);
+
+        let g = graphify(&paths);
+        let d = floyd_warshall_roy(g);
+        dbg!(d);
+    }
+
+    #[test]
+    fn magic() {
+        let input = r"
+        Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+        Valve BB has flow rate=13; tunnels lead to valves CC, AA
+        Valve CC has flow rate=2; tunnels lead to valves DD, BB
+        Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+        Valve EE has flow rate=3; tunnels lead to valves FF, DD
+        Valve FF has flow rate=0; tunnels lead to valves EE, GG
+        Valve GG has flow rate=0; tunnels lead to valves FF, HH
+        Valve HH has flow rate=22; tunnel leads to valve GG
+        Valve II has flow rate=0; tunnels lead to valves AA, JJ
+        Valve JJ has flow rate=21; tunnel leads to valve II".to_string();
+
+        dbg!("Test");
+        dbg!(magical_calculation(&input));
+    }
+
+    #[test]
+    fn array_wizardry() {
+        let thing = vec![vec![u32::MAX / 4; 10]; 10];
+
+        dbg!(thing);
     }
 }
