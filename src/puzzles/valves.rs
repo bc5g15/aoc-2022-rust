9705dc94bc855e457fc01c6e5594a8fce4663fbf
Benedict Cairns <bcairns21@gmail.com>
2023-04-29 11:05:28 +0100

    Remove unused and commented code

--- src/puzzles/valves.rs
+++ src/puzzles/valves.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use std::cmp::max;
 
 type FlowMap = HashMap<String, u32>;
 type PathMap = HashMap<String, Vec<String>>;
@@ -38,84 +37,8 @@ fn read_valves(input: &String) -> (FlowMap, PathMap) {
     (flow_map, path_map)
 }
 
-// Type Node = (Name, Time passed, Pressure released, Valves opened, route)
-type Node = (String, u32, u32, Vec<String>, Vec<String>);
-
-fn best_time_pressure(input: &String) -> u32 {
-    let (flows, paths) = read_valves(input);
-    
-    let mut nodes: Vec<Node> = vec![("AA".to_string(), 0, 0, Vec::new(), Vec::new())];
-
-    let mut max_score: u32 = 0;
-    let mut best_score_at_minute: HashMap<u32, u32> = HashMap::new();
-    let mut best_path: Vec<String> = Vec::new();
-
-    
-    while nodes.len() > 0 {
-        let (address, time, score, open, path) = nodes.pop().unwrap();
-        // dbg!(nodes.len(), &time);
-
-        // if we've hit the time limit then compare with the maximum
-        if time > 30 {
-            max_score = max(max_score, score); 
-            best_path = path;
-            continue;
-        }
-
-        let best_minute = best_score_at_minute.get(&time);
-
-        if let Some(v) = best_minute {
-            // Just skip on if we have a better historical score
-            if *v > score {
-                continue;
-            }
-        }
-        best_score_at_minute.insert(time, score);
-        // Calculate the new score (before opening any new valves)
-        let new_score: u32 = score + open.iter().map(|v| flows.get(v).unwrap()).sum::<u32>();
-
-        let mut new_path = path.clone();
-        new_path.push(address.to_string());
-
-        // Add a route for opening the current valve
-        if let Some(v) = flows.get(&address) {
-            if *v > 0 && !open.contains(&address) {
-                let mut new_open = open.clone();
-                new_open.push(address.to_string());
-                nodes.push((
-                    address.to_string(),
-                    time+1,
-                    new_score,
-                    new_open,
-                    new_path.clone()
-                ));
-                // continue;
-            }
-        }
-
-        // Add routes for the different passages
-        if let Some(v) = paths.get(&address) {
-            v.iter().for_each(|destination| {
-                nodes.push((
-                    destination.to_string(),
-                    time + 1,
-                    new_score,
-                    open.clone(),
-                    new_path.clone()
-                ))
-            })
-        }
-    }
-    // dbg!(best_score_at_minute);
-    // dbg!(best_path);
-    // dbg!(&max_score);
-    max_score
-}
-
 fn graphify(path_map: &PathMap)  -> (Vec<Vec<u32>>, HashMap<String, usize>) {
     let size = path_map.len();
-
-    // dbg!(path_map);
 
     let idx_map: HashMap<String, usize> = path_map.iter()
         .enumerate()
@@ -178,20 +101,6 @@ pub fn magical_calculation(input: &String, turns: u32) -> (u32, HashMap<u64, u32
 }
 
 pub fn part2(input: &String) -> u32 {
-    // let (flows, paths) = read_valves(input);
-    // let (graph, idx_map) = graphify(&paths);
-    // let distances = floyd_warshall_roy(graph);
-
-    // let scored_nodes: Vec<String> = flows.iter()
-    //     .filter(|(_name, score)| **score>0)
-    //     .map(|(name, _score)| name.clone()).collect();
-    //     // Find optimal path through all point-scoring nodes
-    
-    // let start_index = *idx_map.get("AA").unwrap();
-
-    // let mut memory: HashMap<u64, u32> = HashMap::new();
-
-    // travelling_salesman(&flows, &mut state_flow, &scored_nodes, &distances, &idx_map, 30, 0, start_index, 0);
     let (_, memory) = magical_calculation(input, 26);
 
     let max_flow = memory.iter()
@@ -261,7 +170,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn example_one() {
+    fn part_one_sample() {
         let input = r"
         Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
         Valve BB has flow rate=13; tunnels lead to valves CC, AA
@@ -274,46 +183,7 @@ mod tests {
         Valve II has flow rate=0; tunnels lead to valves AA, JJ
         Valve JJ has flow rate=21; tunnel leads to valve II".to_string();
 
-        assert_eq!(best_time_pressure(&input), 1651);
-    }
-
-    #[test]
-    fn read_test() {
-        let input = r"
-        Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
-        Valve BB has flow rate=13; tunnels lead to valves CC, AA
-        Valve CC has flow rate=2; tunnels lead to valves DD, BB
-        Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
-        Valve EE has flow rate=3; tunnels lead to valves FF, DD
-        Valve FF has flow rate=0; tunnels lead to valves EE, GG
-        Valve GG has flow rate=0; tunnels lead to valves FF, HH
-        Valve HH has flow rate=22; tunnel leads to valve GG
-        Valve II has flow rate=0; tunnels lead to valves AA, JJ
-        Valve JJ has flow rate=21; tunnel leads to valve II".to_string();
-
-        let (_flow, paths) = read_valves(&input);
-
-        let (g, x) = graphify(&paths);
-        let d = floyd_warshall_roy(g);
-        dbg!(d);
-        dbg!(x);
-    }
-
-    #[test]
-    fn magic() {
-        let input = r"
-        Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
-        Valve BB has flow rate=13; tunnels lead to valves CC, AA
-        Valve CC has flow rate=2; tunnels lead to valves DD, BB
-        Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
-        Valve EE has flow rate=3; tunnels lead to valves FF, DD
-        Valve FF has flow rate=0; tunnels lead to valves EE, GG
-        Valve GG has flow rate=0; tunnels lead to valves FF, HH
-        Valve HH has flow rate=22; tunnel leads to valve GG
-        Valve II has flow rate=0; tunnels lead to valves AA, JJ
-        Valve JJ has flow rate=21; tunnel leads to valve II".to_string();
-
-        dbg!(magical_calculation(&input, 30));
+        assert_eq!(part1(&input), 1651);
     }
 
     #[test]
@@ -330,7 +200,7 @@ mod tests {
         Valve II has flow rate=0; tunnels lead to valves AA, JJ
         Valve JJ has flow rate=21; tunnel leads to valve II".to_string();
 
-        dbg!(part2(&input));
+        assert_eq!(part2(&input), 1707);
     }
 
     #[test]
